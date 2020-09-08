c32f0e12d7d3c060d27769512b0b13b1ef6f91a5
Harikalatheeswaran <56271057+Harikalatheeswaran@users.noreply.github.com>
2020-09-08 18:55:18 +0530

    Update DFT_&_IDFT.py

--- DFT_&_IDFT.py
+++ DFT_&_IDFT.py
@@ -1,6 +1,7 @@
 import math
+import cmath
 
-p = math.pi
+pi = cmath.pi
 choice = input('1 for DFT....2 for IDFT :\n')
 
 if choice == '1':
@@ -10,19 +11,17 @@ if choice == '1':
    b = a.split(',')
    x = []
    for i in b:
-      i = complex(i)
+      i = float(i)
       x.append(i)
    N = len(x)
    x_out = []
-   x_temp = 0+0j
+   x_temp = complex()
 
    for k in range(0,N):
       for n in range(0,N):
-         c = math.cos((2*p*k*n)/N)
-         s = math.sin((2*p*k*n)/N)
-         c = round(c)
-         s = round(s)
-         m = complex(c,-s)
+         
+         e = cmath.exp((-2*pi*n*k*1j)/N)
+         m = complex(round(e.real),round(e.imag))
          
          x_temp = x_temp + (x[n]*m)
       x_out.append(x_temp)
@@ -46,22 +45,17 @@ elif choice == '2':
 
    for k in range(0,N):
       for n in range(0,N):
-         c = math.cos((2*p*k*n)/N)
-         s = math.sin((2*p*k*n)/N)
-         c = round(c)
-         s = round(s)
-         m = complex(c,s)
+        
+         e = cmath.exp((2*pi*n*k*1j)/N)
+         m = complex(round(e.real),round(e.imag))
          
          x_temp = x_temp + (x[n]*m)
       x_out.append((x_temp)/N)
       x_temp = 0
    print('\n   The Values :   \n')
    for i in x_out:
-      print(i)
+      print(i.real)
 
 else:
    print('DUDE ?!')
-
-
-
 
